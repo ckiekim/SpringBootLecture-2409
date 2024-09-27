edc490b394b068ce874fe4dcb0e3d051823b21ba
Cheolkie Kim <ckiekim@naver.com>
2024-09-27 10:12:09 +0900

    Book

--- src/main/java/com/hae/demo/controller/BookController.java
+++ src/main/java/com/hae/demo/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +22,35 @@ public class BookController {
         List<Book> bookList = bookService.getBookList();
         model.addAttribute("bookList", bookList);
         return "book/list";
+    }
+
+    @GetMapping("/insert")
+    public String insertForm() {
+        return "book/insert";
+    }
+
+    @PostMapping("/insert")
+    public String insertProc(Book book) {
+        bookService.insertBook(book);
+        return "redirect:/book/list";
+    }
+
+    @GetMapping("/update/{bid}")
+    public String updateForm(@PathVariable int bid, Model model) {
+        Book book = bookService.getBook(bid);
+        model.addAttribute("book", book);
+        return "book/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProc(Book book) {
+        bookService.updateBook(book);
+        return "redirect:/book/list";
+    }
+
+    @GetMapping("/delete/{bid}")
+    public String delete(@PathVariable int bid) {
+        bookService.deleteBook(bid);
+        return "redirect:/book/list";
     }
 }
